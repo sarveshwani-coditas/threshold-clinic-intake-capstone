7842afdf3ae2d0ec0cc7493d548dd6f6e6149735
Sarvesh Wani <sarvesh.wani@coditas.com>
2026-06-24 11:41:18 +0530

    Initial commit

---
+++ src/test/java/com/coditas/thresholdclinicintakecapstone/ThresholdClinicIntakeCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coditas.thresholdclinicintakecapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThresholdClinicIntakeCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
